77b2611f19f9a8041f9be8bd7eb3ab4190df5b73
Siddu <siddusthawarmath@gmail.com>
2018-10-03 12:08:22 +0530

    guva cache

--- src/main/java/org/sample/app/SpringbootCacheGuavaApp.java
+++ src/main/java/org/sample/app/SpringbootCacheGuavaApp.java
@@ -1,20 +1,12 @@
 package org.sample.app;
 
-import java.util.concurrent.TimeUnit;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
-import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.cache.concurrent.ConcurrentMapCacheManager;
-import org.springframework.cache.guava.GuavaCacheManager;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-
-import com.google.common.cache.CacheBuilder;
 
 @SpringBootApplication
 @EnableAutoConfiguration
